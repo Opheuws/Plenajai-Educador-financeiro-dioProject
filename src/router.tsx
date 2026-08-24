9e5a123221e3a23fe035da4ae6bd12025fa169c3
Opheuws <eduarllemprates930@gmail.com>
2026-08-24 09:27:56 -0300

    fix: injeta basename para rotas funcionarem no github pages

--- src/router.tsx
+++ src/router.tsx
@@ -5,22 +5,27 @@ import { SimulationFormPage } from './pages/SimulationFormPage'
 import { SimulationResultsPage } from './pages/SimulationResultsPage'
 import { SimulationHistoryPage } from './pages/SimulationHistoryPage'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: <RootLayout />,
+      children: [
+        {
+          path: '/',
+          element: <SimulationFormPage />,
+        },
+        {
+          path: '/resultado/:id',
+          element: <SimulationResultsPage />,
+        },
+        {
+          path: '/historico',
+          element: <SimulationHistoryPage />,
+        },
+      ],
+    },
+  ],
   {
-    element: <RootLayout />,
-    children: [
-      {
-        path: '/',
-        element: <SimulationFormPage />,
-      },
-      {
-        path: '/resultado/:id',
-        element: <SimulationResultsPage />,
-      },
-      {
-        path: '/historico',
-        element: <SimulationHistoryPage />,
-      },
-    ],
+    basename: '/Plenajai-Educador-financeiro-dioProjeto', // 🛠️ Linha mágica adicionada aqui!
   },
-])
+)
